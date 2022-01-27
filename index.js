dd690d82e69aaeed3d3a10020bc5813bb7e1af83
Nazmus Sakib <nazmussakibppp22@gmail.com>
2022-01-27 23:45:32 +0600

    validation

--- index.js
+++ index.js
@@ -24,16 +24,8 @@ async function run() {
         const database = client.db("crazytraveler");
         const blogCollections = database.collection("blogs");
         const userCollections = database.collection("users");
-        // const ordersCollection = database.collection("orders");
-        // const usersCollection = database.collection("users");
-        // const reviewsCollection = database.collection("reviews");
 
         // // Get Api 
-        // app.get('/blogs', async (req, res) => {
-        //     const products = blogCollections.find({});
-        //     const result = await products.toArray();
-        //     res.send(result);
-        // })
         app.get('/blogs/:id', async (req, res) => {
             const id = req.params.id;
             const blogs = { _id: ObjectId(id) };
@@ -51,24 +43,6 @@ async function run() {
             }
             res.json({ admin: isAdmin })
         })
-        // app.get('/orders', async (req, res) => {
-        //     const orders = ordersCollection.find({});
-        //     const result = await orders.toArray();
-        //     res.send(result);
-        // })
-        // app.get('/orders', async (req, res) => {
-        //     const email = req.query.email;
-        //     console.log(email);
-        //     const query = { email: email };
-        //     const myOrders = ordersCollection.find(query);
-        //     const result = await myOrders.toArray();
-        //     res.send(result);
-        // })
-        // app.get('/reviews', async (req, res) => {
-        //     const reviews = reviewsCollection.find({});
-        //     const result = await reviews.toArray();
-        //     res.send(result);
-        // })
 
         app.get('/blogs', async(req, res) => {
             const cursor = blogCollections.find({});
@@ -95,6 +69,7 @@ async function run() {
             const time = req.body.time;
             const ratings = req.body.ratings;
             const expense = req.body.expense;
+            const status = req.body.status;
             const img = req.files.image;
             const imgData = img.data;
             const encodedData = imgData.toString('base64');
@@ -108,69 +83,36 @@ async function run() {
                 ratings,
                 expense,
                 image: imgBuffer,
-                status: false
+                status
             }
             const result = await blogCollections.insertOne(blog)
             res.json(result)
         })
 
-        // app.post('/orders', async (req, res) => {
-        //     const orders = req.body;
-        //     const result = await ordersCollection.insertOne(orders);
-        //     res.json(result);
-        // })
-        // app.post('/users', async (req, res) => {
-        //     const addUser = req.body;
-        //     const result = await usersCollection.insertOne(addUser);
-        //     res.json(result);
-        // })
-        // app.post('/reviews', async (req, res) => {
-        //     const reviews = req.body;
-        //     const result = await reviewsCollection.insertOne(reviews);
-        //     res.json(result);
-        // })
-        // app.post('/blogs', async (req, res) => {
-        //     const addPd = req.body;
-        //     const result = await blogCollections.insertOne(addPd);
-        //     res.json(result);
-        // })
-        // // Put Api
-        // app.put('/users/admin', async (req, res) => {
-        //     const user = req.body;
-        //     const filter = { email: user.email };
-        //     const updateDoc = { $set: { role: 'admin' } }
-        //     const result = await usersCollection.updateOne(filter, updateDoc);
-        //     res.json(result);
-        // })
-        // app.put('/orders/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const updateInfo = req.body;
-        //     console.log(updateInfo);
-        //     const filter = { _id: ObjectId(id) };
-        //     const options = { upsert: true };
-        //     const updateDoc = {
-        //         $set: {
-        //             status: 'Shipped'
-        //         }
-        //     }
-        //     const result = await ordersCollection.updateOne(filter, updateDoc, options);
-        //     console.log(result);
+        //Put Api
+        app.put('/blogs/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateInfo = req.body;
+            console.log(updateInfo);
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    status: 'approved'
+                }
+            }
+            const result = await blogCollections.updateOne(filter, updateDoc, options);
+            console.log(result);
 
-        //     res.json(result);
-        // })
-        // // Delete Api
-        // app.delete('/orders/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const deletedItem = { _id: ObjectId(id) };
-        //     const result = await ordersCollection.deleteOne(deletedItem);
-        //     res.json(result);
-        // })
-        // app.delete('/blogs/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const deletedItem = { _id: ObjectId(id) };
-        //     const result = await blogCollections.deleteOne(deletedItem);
-        //     res.json(result);
-        // })
+            res.json(result);
+        })
+        // Delete Api
+        app.delete('/blogs/:id', async (req, res) => {
+            const id = req.params.id;
+            const deletedItem = { _id: ObjectId(id) };
+            const result = await blogCollections.deleteOne(deletedItem);
+            res.json(result);
+        })
 
 
         console.log('DB conected');
